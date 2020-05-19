5d6037d21b0eaac2aaf5cf6ae23519d6939a51cd
Gustavo <gustavosohne38@gmail.com>
2020-05-19 17:13:22 -0300

    sssssss

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import cors from 'cors'
 import express from 'express'
 import versionList from './routes/indexRoutes'
+import dotenv from 'dotenv'
 
-const PORT = 3000
+dotenv.config()
 
 const app = express()
+
+let port = process.env.PORT
+
+if (port == null || port === '') {
+  port = 8000
+}
 
 app.use(express.json())
 app.use(cors())
 app.use('/', versionList)
 
-app.listen(PORT, () => {
-  console.log(`Server running at port  mode`)
+app.listen(port, () => {
+  // console.log(`Server running at port  mode`)
 })
 export default app
